1de92d15512ba706d2426dc376ea0f25a08c191d
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-02-24 04:49:18 +0200

    [+?] [me_Counters] Timekeeping and Delay_ms(), Delay_us().

--- examples/me_Counters/me_Counters.ino
+++ examples/me_Counters/me_Counters.ino
@@ -6,7 +6,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2025-02-23
+  Last mod.: 2025-02-24
 */
 
 #include <me_Counters.h>
@@ -15,115 +15,252 @@
 #include <me_Uart.h>
 #include <me_Console.h>
 
-// extern "C" void __vector_10() __attribute__((interrupt, used));
+static volatile TUint_4 NumSecondsPassed = 0;
+static volatile TUint_2 MilliSecondsPart = 0;
+static me_Counters::TCounter2 Rtc;
 
-struct TLineState
+const TUint_2 MaxSeconds = 10;
+
+struct TTimestamp
 {
-  TBool IsLow;
-  TBool IsSensibleMark;
-  TUint_2 StartMark;
+  TUint_2 S;
+  TUint_2 Ms;
+  TUint_2 Us;
 };
 
-TBool GetLineChange(
-  TLineState * Signal
+const TTimestamp ZeroTimestamp = { 0, 0, 0 };
+
+void PrintTimestamp(
+  TTimestamp Ts
 )
 {
-  me_Counters::TCounter2 CapturingCounter;
+  Console.Write("Timestamp (");
+  Console.Print(Ts.S);
+  Console.Print(Ts.Ms);
+  Console.Print(Ts.Us);
+  Console.Write(")");
+  Console.EndLine();
+}
 
-  TBool IsUnknowSignalLength = false;
+TTimestamp ToTimestamp(
+  TUint_2 S,
+  TUint_2 Ms,
+  TUint_2 Us
+)
+{
+  TTimestamp Result;
 
-  if (CapturingCounter.Status->GotEventMark)
+  Result.S = S;
+  Result.Ms = Ms;
+  Result.Us = Us;
+
+  return Result;
+}
+
+TTimestamp MicrosToTimestamp(
+  TUint_2 NumMicros
+)
+{
+  TTimestamp Result;
+
+  TUint_2 Remainder = NumMicros;
+
+  Result.Us = Remainder % 1000;
+
+  Remainder /= 1000;
+
+  Result.Ms = Remainder % 1000;
+
+  Remainder /= 1000;
+
+  if (Remainder > MaxSeconds)
+    Remainder = MaxSeconds;
+
+  Result.S = Remainder;
+
+  return Result;
+}
+
+TTimestamp MillisToTimestamp(
+  TUint_2 NumMills
+)
+{
+  TTimestamp Result;
+
+  TUint_2 Remainder = NumMills;
+
+  Result.Us = 0;
+
+  Result.Ms = Remainder % 1000;
+
+  Remainder /= 1000;
+
+  if (Remainder > MaxSeconds)
+    Remainder = MaxSeconds;
+
+  Result.S = Remainder;
+
+  return Result;
+}
+
+TBool AddTimestamp(
+  TTimestamp * Dest,
+  TTimestamp Ts
+)
+{
+  Dest->Us += Ts.Us;
+  Dest->Ms += Ts.Ms;
+  Dest->S += Ts.S;
+
+  if (Dest->Us >= 1000)
   {
-    Signal->IsLow = !CapturingCounter.Control->EventIsOnUpbeat;
-
-    if (CapturingCounter.Status->Done)
-    {
-      IsUnknowSignalLength = true;
-      CapturingCounter.Status->Done = true; // Yep, cleared by writing one
-    }
-
-    if (IsUnknowSignalLength)
-    {
-      Signal->IsSensibleMark = false;
-      Signal->StartMark = 0;
-    }
-    else
-    {
-      Signal->IsSensibleMark = true;
-      Signal->StartMark = *CapturingCounter.EventMark;
-    }
-
-    (*CapturingCounter.Current) = 0;
-
-    CapturingCounter.Control->EventIsOnUpbeat =
-      !CapturingCounter.Control->EventIsOnUpbeat;
-
-    CapturingCounter.Status->GotEventMark = true; // Also cleared by writing one
-
-    return true;
+    Dest->Us -= 1000;
+    ++Dest->Ms;
   }
+
+  if (Dest->Ms >= 1000)
+  {
+    Dest->Ms -= 1000;
+    ++Dest->S;
+  }
+
+  if (Dest->S >= MaxSeconds)
+  {
+    Dest->S -= MaxSeconds;
+    return false;
+  }
+
+  return true;
+}
+
+TBool TimestampIsBefore(
+  TTimestamp CurrentTs,
+  TTimestamp BorderTs
+)
+{
+  if (CurrentTs.S < BorderTs.S)
+    return true;
+
+  if (CurrentTs.S > BorderTs.S)
+    return false;
+
+  if (CurrentTs.Ms < BorderTs.Ms)
+    return true;
+
+  if (CurrentTs.Ms > BorderTs.Ms)
+    return false;
+
+  if (CurrentTs.Us < BorderTs.Us)
+    return true;
 
   return false;
 }
 
-TUint_2 GetUsFromTicks(
-  TUint_2 NumTicks
+TBool TimestampIsAfter(
+  TTimestamp CurrentTs,
+  TTimestamp BorderTs
 )
 {
-  const TUint_2 Prescale = 64;
-  return (TUint_4) (1000000 / (F_CPU / Prescale)) * NumTicks;
+  return !TimestampIsBefore(CurrentTs, BorderTs);
+}
+
+TUint_2 GetMicrosPart()
+{
+  return (TUint_4) (*Rtc.Current) * 1000 / 249;
+}
+
+TTimestamp GetTimestamp()
+{
+  return ToTimestamp(NumSecondsPassed, MilliSecondsPart, GetMicrosPart());
+}
+
+void WaitTill(
+  TTimestamp Ts
+)
+{
+  while (TimestampIsBefore(GetTimestamp(), Ts));
+}
+
+void WaitAfter(
+  TTimestamp Ts
+)
+{
+  while (TimestampIsAfter(GetTimestamp(), Ts));
+  WaitTill(Ts);
+}
+
+void Delay_us(
+  TUint_2 NumMicroseconds
+)
+{
+  TTimestamp DeltaTs = MicrosToTimestamp(NumMicroseconds);
+
+  TTimestamp EndTs = GetTimestamp();
+
+  if (!AddTimestamp(&EndTs, DeltaTs))
+    WaitAfter(EndTs);
+  else
+    WaitTill(EndTs);
+}
+
+void Delay_ms(
+  TUint_2 NumMilliseconds
+)
+{
+  TTimestamp DeltaTs = MillisToTimestamp(NumMilliseconds);
+
+  TTimestamp EndTs = GetTimestamp();
+
+  if (!AddTimestamp(&EndTs, DeltaTs))
+    WaitAfter(EndTs);
+  else
+    WaitTill(EndTs);
+}
+
+extern "C" void __vector_11() __attribute__((interrupt, used));
+
+// Interrupt 11 is for counter 2 mark A event
+void __vector_11()
+{
+  ++MilliSecondsPart;
+
+  if (MilliSecondsPart == 1000)
+  {
+    MilliSecondsPart = 0;
+    ++NumSecondsPassed;
+
+    if (NumSecondsPassed == MaxSeconds)
+      NumSecondsPassed = 0;
+  }
 }
 
 void setup()
 {
-  me_Uart::Init(me_Uart::Speed_1M_Bps);
+  me_Uart::Init(me_Uart::Speed_115k_Bps);
 
   {
-    using
-      me_Counters::TCounter2,
-      me_Counters::TAlgorithm_Counter2,
-      me_Counters::TDriveSource_Counter2;
+    using namespace me_Counters;
 
-    TCounter2 CapturingCounter;
-
-    CapturingCounter.SetAlgorithm(TAlgorithm_Counter2::Count_To2Pow16);
-    CapturingCounter.Control->DriveSource = (TUint_1) TDriveSource_Counter2::Internal_SlowBy2Pow6;
-    (*CapturingCounter.Current) = 0;
-    CapturingCounter.Control->EventIsOnUpbeat = false;
+    Rtc.SetAlgorithm(TAlgorithm_Counter2::Count_ToMarkA);
+    Rtc.Control->DriveSource = (TUint_1) TDriveSource_Counter2::Internal_SlowBy2Pow6;
+    Rtc.Control->PinActionOnMarkA = (TUint_1) TPinAction::Toggle;
+    *Rtc.Current = 0;
+    *Rtc.MarkA = 249;
+    Rtc.Wiring->EnableMarkAInterrupt = true;
   }
 
-  Console.Print("Init done");
+  const TUint_1 OutputPin = 9;
+  pinMode(OutputPin, OUTPUT);
+
+  Console.Print("Init done.");
 }
 
 void loop()
 {
-  TLineState LineState;
-  TBool IsTic;
+  Delay_ms(1373);
+  // Delay_us(653);
+  // delay(1000);
 
-  if (GetLineChange(&LineState))
-  {
-    IsTic = LineState.IsLow;
-
-    if (IsTic)
-      Console.Write("Tic");
-    else
-      Console.Write("Tac");
-
-    if (LineState.IsSensibleMark)
-      Console.Print(LineState.StartMark);
-    else
-      Console.Write("     ? ");
-
-    if (IsTic)
-      Console.Write(" ");
-    else
-      Console.EndLine();
-  }
+  PrintTimestamp(GetTimestamp());
+  // Console.Print("Tic");
 }
-
-/*
-  2024-12-29
-  2024-12-30
-  2025-01-01
-  2025-01-05
-*/
